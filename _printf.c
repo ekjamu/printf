1b702846fdd6d7fa82ca8364b29b6f63cd5e7191
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-10 21:07:50 -0800

    update printf source code and fixing errors

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _printf - will duplicate the printf function
@@ -11,5 +12,38 @@
 int _printf(const char *format, ...)
 {
 /*	this will be our super awesome function!!*/
-	
+	va_list print;
+	unsigned int i = 0, count = 0;
+	char *formatCpy;
+	t_mod selector[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_int},
+		{"f", print_float},
+		{"o", print_eight},
+		{"x", print_sixteen},
+		{NULL, NULL}
+	};
+
+	formatCpy = _strdup(format)
+	va_start(print, format);
+	while (formatCpy != NULL && (*(formatCpy + i)) != '\0')
+	{
+		count = 0;
+		while (selector[count].symbol != NULL)
+		{
+			if (*(selector[count].symbol) == *(formatCpy + i))
+			{
+				if (*(formatCpy - 1) == "%")
+				{
+					printf("found one\n");
+				}
+			}
+			count++;
+		}
+		i++;
+	}
+	va_end(print);
+	putchar('\n');
 }
--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
 /**
 * struct forms - holds my selector and pointer to function
