12288bc4939f66595cf01e0ca8df8bb991d57258
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-10 20:30:49 -0800

    adding base printf source code

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _printf - will duplicate the printf function
+ * @format: the string we want manipulate
+ * Description: long description
+ *
+ * Return: count of the length printed to screen
+ */
+
+int _printf(const char *format, ...)
+{
+/*	this will be our super awesome function!!*/
+	
+}
