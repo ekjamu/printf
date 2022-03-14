5ddffa367efe38792ae1f9c79d8232dbb8c06f90
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-14 13:39:26 -0700

    a folder containing our test files

---
+++ test/test.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    char *s = "hello";
+    _printf("Hi guys,\n");
+    _printf("hi guys %s bob\n", s);
+    _printf("sup guys %o typop\n", 8);
+    _printf("oct test: %x\n", -1553);
+    _printf("int test %i\n", -1414);
+   return (0);
+}
