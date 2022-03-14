12646f68a38406a33d2d8eec7d3f9500904d249d
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-13 18:05:09 -0700

    adding primary version of print_sixteen

--- print_sixteen.c
+++ print_sixteen.c
@@ -9,5 +9,39 @@
   */
 char *print_sixteen(char *format, va_list *var)
 {
+	char *formatEnd;
+	unsigned int number = 0;
+	char printNum[20], i = 0, k = 0;
+	char *convert = "0123456789abcdef";
+	number = va_arg(*var, unsigned int);
+
+	/* perhaps we should exit here instead as printf wouldn't compile */
+	formatEnd = malloc(sizeof(*formatEnd) * (_strlen(format) + 1));
+	if (formatEnd == NULL)
+		return (format);
+	if (number < 0)
+	{
+		number = number * -1;
+		printNum[i] = '-';
+		i++;
+	}
+	while (number > 15)
+	{
+		printNum[i] = conver[number % 16];
+		i++;
+		number = number / 16;
+	}
+	printNum[i] = convert[number % 16];
+	i++;
+	printNum[i] = '\0';
+	_strcpy(formatEnd, format);
+	_strcpy(format, printNum);
+
+	format = format + _strlen(printNum);
+
+	if (_strlen(formatEnd) > 2)
+		_strcpy(format, formatEnd + 2);
+
+	free(formatEnd);
 	return (format);
 }
