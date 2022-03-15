8bbb0b617fb24bb46e6eb4258f37b68fee39fb58
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-14 19:37:49 -0700

    changing code to match unsi.c code bu handle neg

--- print_int.c
+++ print_int.c
@@ -12,42 +12,44 @@
 char *print_int(char *format, va_list *var)
 {
 	char *formatEnd;
-	char intToChar[32];
-	int argInt, orgInt, i = 0, a = 0;
+	unsigned long int number = 0, i = 0;
+	char printNum[20];
+	char *convert = "0123456789";
+	int initial_num;
 
-	argInt = va_arg(*var, int);
-	orgInt = argInt;
+	initial_num = va_arg(*var, unsigned long int);
+
+	if (initial_num < 0)
+		number = initial_num * -1;
+	else
+		number = initial_num;
 	formatEnd = malloc(sizeof(*formatEnd) * (_strlen(format) + 1));
 	if (formatEnd == NULL)
-		return (format);
+		return (NULL);
+
+	while (number > 9)
+	{
+		*(printNum + i) = *(convert + (number % 10));
+		i++;
+		number = number / 10;
+	}
+	*(printNum + i) = *(convert + (number % 10));
+	i++;
+	if (initial_num < 0)
+	{
+		*(printNum + i) = '-';
+		i++;
+	}
+	*(printNum + i) = '\0';
+	rev_string(printNum);
 	_strcpy(formatEnd, format);
+	_strcpy(format, printNum);
 
-	if (orgInt < 0)
-		argInt = -argInt;
+	format = format + _strlen(printNum);
 
-	while (argInt > 9)
-	{
-		intToChar[i] = argInt % 10 + 48;
-		argInt = argInt / 10;
-		i++;
-	}
-	intToChar[i] = argInt + 48;
-	if (orgInt < 0)
-	{
-		i++;
-		intToChar[i] = '-';
-	}
-	while (i >= 0)
-	{
-		*(format + a) = intToChar[i];
-		a++;
-		i--;
-	}
-	format = format + a;
 	if (_strlen(formatEnd) > 2)
 		_strcpy(format, formatEnd + 2);
-	else
-		*format = '\0';
+
 	free(formatEnd);
 	return (format);
 }
